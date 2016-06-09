e4d11cce62c3df5658479fe4284a90479b20a920
mailsspencer <simons.jodis@gmail.com>
2016-06-09 22:08:04 +0100

    done
    Looks good

--- src/main.c
+++ src/main.c
@@ -14,39 +14,38 @@ static Window *window;
 static TextLayer *text_top;
 static TextLayer *text_layer;
 static Layer *s_canvas_layer;
-static int StatusBar = 0;
+static int StatusHour = 0;
+static int StatusMin = 0;
+static char szDay[10];
+static char szDate[20];
+static char s_buffer_date[30];
+
 int anglev, x, y, a;
 int radiusv;
 
-double pi(void)
-{
-  return 3.141592653589793;
-}
 
-float rad2deg(float angle)
-{
-  return angle * (180.0f / pi());  
-}
-
-float deg2rad(float angle)
-{
-  return angle * (pi() / 180.0f);  
-}
 
 static void update_time() 
 {
   time_t temp = time(NULL);
   struct tm *tick_time = localtime(&temp);
-  static char s_buffer2[8];
-  static char s_buffer3[20];
-  strftime(s_buffer2, sizeof(s_buffer2), "%H:%M", tick_time);
+ // static char s_buffer2[8];
+ // static char s_buffer3[20];
+ // strftime(s_buffer2, sizeof(s_buffer2), "%H:%M", tick_time);
   
-  strftime(s_buffer3, sizeof(s_buffer3), "%a %d/%m/%y", tick_time);
+  strftime(szDay, sizeof(szDay), "%a", tick_time);
+  strftime(szDate, sizeof(szDate), "%d/%m/%y", tick_time);
   
-  text_layer_set_text(text_top, s_buffer3);
-  text_layer_set_text(text_layer, s_buffer2);
+  snprintf(s_buffer_date, 22, "\r\n\r\n%s\r\n%s", szDay, szDate);
   
-  StatusBar = tick_time->tm_min + (tick_time->tm_hour * 60);
+//  text_layer_set_text(text_top, s_buffer3);
+//  text_layer_set_text(text_layer, s_buffer2);
+  
+  StatusHour = tick_time->tm_hour;
+  if (StatusHour >= 12)
+    StatusHour -= 12;
+    
+  StatusMin = tick_time->tm_min; 
   
   layer_mark_dirty(s_canvas_layer);
 }
@@ -59,10 +58,14 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed)
 
 static void canvas_update_proc(Layer *layer, GContext *ctx) 
 {
+  float AngleHour = 0;
+  float AngleMin = 0;
+
+  /*
   int i;
   radiusv = 150;
 
-  for( i = 1 ; i < StatusBar + 1 ; i++)
+  for( i = 1 ; i < StatusHour + 1 ; i++)
   {
     anglev = TRIG_MAX_ANGLE * i / MAX_MINS;
 
@@ -73,6 +76,27 @@ static void canvas_update_proc(Layer *layer, GContext *ctx)
     GPoint end = GPoint(DISP_X_MID + x, DISP_Y_MID + y);
     graphics_draw_line(ctx, start, end); 
   }
+  */
+  GRect bounds = layer_get_bounds(layer);
+  
+    // Date
+  graphics_context_set_text_color(ctx, GColorBlack);
+  graphics_draw_text(ctx, s_buffer_date, fonts_get_system_font(FONT_KEY_GOTHIC_24), bounds,
+                     GTextOverflowModeWordWrap, GTextAlignmentCenter, NULL);
+  
+  // Minute
+  AngleMin = (360.0f * ((float)StatusMin / 60.0f));
+  GRect frame = grect_inset(bounds, GEdgeInsets(-40));
+  graphics_context_set_fill_color(ctx, GColorBlack);
+  graphics_fill_radial(ctx, frame, GOvalScaleModeFitCircle, 45,
+                                  DEG_TO_TRIGANGLE(0), DEG_TO_TRIGANGLE(AngleMin));
+  
+  // Hour
+  AngleHour = (360.0f * ((float)StatusHour / 12.0f));
+      GRect frame2 = grect_inset(bounds, GEdgeInsets(10));
+  graphics_context_set_fill_color(ctx, GColorBlack);
+  graphics_fill_radial(ctx, frame2, GOvalScaleModeFitCircle, 20,
+                                  DEG_TO_TRIGANGLE(0), DEG_TO_TRIGANGLE(AngleHour));
 }
 
 
@@ -81,6 +105,7 @@ static void window_load(Window *window)
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
+  /*
   text_top = text_layer_create((GRect) { .origin = { 5, 10 }, .size = { bounds.size.w-10, 40 } });
   text_layer_set_text_alignment(text_top, GTextAlignmentCenter);
   text_layer_set_font(text_top, fonts_get_system_font(FONT_KEY_GOTHIC_28));
@@ -88,7 +113,7 @@ static void window_load(Window *window)
   text_layer = text_layer_create((GRect) { .origin = { 10, 90 }, .size = { bounds.size.w-20, 50 } });
   text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
   text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_MEDIUM_NUMBERS));
-  
+  */
   // Create canvas layer
   s_canvas_layer = layer_create(bounds);
   layer_set_update_proc(s_canvas_layer, canvas_update_proc);
@@ -96,8 +121,8 @@ static void window_load(Window *window)
   update_time();
   
   layer_add_child(window_get_root_layer(window), s_canvas_layer);
-  layer_add_child(window_layer, text_layer_get_layer(text_top));
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+//  layer_add_child(window_layer, text_layer_get_layer(text_top));
+//  layer_add_child(window_layer, text_layer_get_layer(text_layer));
 }
 
 
